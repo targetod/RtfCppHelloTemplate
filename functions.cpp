8075df0838bfb1f1ee7ed8c97fd78cf563e91529
targetod <dragon_sasha@meta.ua>
2022-02-13 22:47:50 +0200

    u1

--- functions.cpp
+++ functions.cpp
@@ -1,9 +1,9 @@
-
+// Please correct the function according to Task.
 
 #include "functions.h"
 
 
-
-std::string printHello() {
-	return "hello RTF";
+std::string printHello() 
+{
+	return "hello  xxx";
 }
